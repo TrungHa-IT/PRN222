c4aa0227b69e729a9bc37bdbd9ffb18fad4df83a
TrungHa-IT <hatrung03022003@gmail.com>
2025-01-17 11:54:40 +0700

    Add slot 3_1

---
+++ PRN222-Slot3_Demo1/TaskDemonstration01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program {
+    static void printNumber(string message)
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine($"{message}:{i}");
+            Thread.Sleep(1000);
+        }
+    }
+    static void Main() {
+        Thread.CurrentThread.Name = "Main";
+        Task task01 = new Task(() => printNumber("Task 01"));
+        task01.Start();
+        Task task02 = Task.Run(delegate{printNumber("Task 02");});
+        Task task03 = new Task(new Action(() => printNumber("Task 03")));
+        task03.Start();
+        Console.WriteLine($"Thread: '{Thread.CurrentThread.Name}'");
+        Console.ReadKey();
+    }
+}
